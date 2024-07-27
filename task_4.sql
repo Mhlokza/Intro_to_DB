2f97d112348b680a8b46249c30dc0ce723c41228
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 21:40:55 +0200

    task 4

--- task_4.sql
+++ task_4.sql
@@ -4,6 +4,7 @@ mydb = mysql.connector.connect(host="localhost",user="mhlonipheni",psssword="Mag
 USE alx_book_store;
 mycursor =mydb.cursor()
 mycursor.execute("SELECT * FROM books")
+INFORMATION_SCHEMA.COLUMNS "COLUMN_NAME", "COLUMN_TYPE", TABLE_SCHEMA = 'alx_book_store' TABLE_NAME = 'Books'
 myresult =mycursor.fetchall()
 
 for row in myresult:
