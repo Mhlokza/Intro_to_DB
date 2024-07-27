731e49264e9d747e96e7b7225bcdab5436b07403
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 17:57:55 +0200

    sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,7 +6,7 @@ mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 USE alx_book_store
 
 CREATE TABLE Book(
-book_id (Primary Key)
+book_id (PRIMARY KEY)
 title VARCHAR(130)
 author_id (Foreign Key referencing Authors table)
 price DOUBLE
@@ -14,26 +14,26 @@ publication_date DATE
 )
 
 CREATE TABLE Authors(
-author_id (Primary Key)
+author_id (PRIMARY KEY)
 author_name VARCHAR(215)
 )
 
 CREATE TABLE Customers(
-customer_id (Primary Key)
+customer_id (PRIMARY KEY)
 customer_name VARCHAR(215)
 email VARCHAR(215)
 address TEXT
 )
 
 CREATE TABLE Orders(
-order_id (Primary Key)
+order_id (PRIMARY KEY)
 customer_id (Foreign Key referencing Customers table)
 order_date DATE
 )
 
 CREATE TABLE Order_Details(
-orderdetailid (Primary Key)
-order_id (Foreign Key referencing Orders table)
+orderdetailid (PRIMARY KEY)
+order_id (FOREIGN KEY referencing Orders table)
 book_id (Foreign Key referencing Books table)
 quantity DOUBLE
 )
