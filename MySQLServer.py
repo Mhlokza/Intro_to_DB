c6f070df0a00fb0a2a17db4364402edf6b5606ab
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 18:43:24 +0200

    mysql connection to python

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,6 @@
-import sql.connector
+import mysql.connector
 
-mydb = sql.connector.connect(host="localhost",user="mhlonipheni",psssword="Magagula@123",)
+mydb = mysql.connector.connect(host="localhost",user="mhlonipheni",psssword="Magagula@123",)
 mycursor =mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 print("Database 'alx_book_store' created successfully!")
