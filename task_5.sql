54cf697efd48e4b40f60b80adc97b48226121440
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 22:02:00 +0200

    task 5

--- task_5.sql
+++ task_5.sql
@@ -7,4 +7,4 @@ mycursor.execute("INSERT INTO customer(customer_id = 1
                                        customer_name,
                                        email,
                                        address) VALUE(1,
-                                       Cole Baidoo,cbaidoo@sandtech.com,123 Happiness Ave)")
+                                       Cole Baidoo,cbaidoo@sandtech.com,123 Happiness Ave.)")
