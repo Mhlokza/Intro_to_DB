61736c59e9c7fd809e39fec0a80a2bcff19f589d
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 19:41:37 +0200

    task 3

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost",user="mhlonipheni",psssword="Magagula@123",database = "lx_book_store")
+mycursor = mydb.cursor()
+mycursor.execute("FROM lx_book_store SHOW TABLES ")
+for tables in mycursor:
+print(mycursor)
