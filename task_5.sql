50b33ed175d72a639dd6881c0e1242366f0681a7
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 21:59:22 +0200

    task 5

---
+++ task_5.sql
@@ -0,0 +1,10 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost",user="mhlonipheni",psssword="Magagula@123",database = "lx_book_store")
+USE alx_book_store;
+mycursor =mydb.cursor()
+mycursor.execute("INSERT INTO customer(customer_id = 1
+                                       customer_name,
+                                       email,
+                                       address) VALUE(1,
+                                       Cole Baidoo,cbaidoo@sandtech.com,123 Happiness Ave)")
