064cf64c962274b3fbe41889ca1a65ccc79dc25d
Gabisile Nkosi <gabisile.nkosi@standardbank.co.za>
2024-07-27 22:22:43 +0200

    task 6

--- task_6.sql
+++ task_6.sql
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost",user="mhlonipheni",psssword="Magagula@123",database = "lx_book_store")
 USE alx_book_store;
 mycursor =mydb.cursor()
-mycursor.execute("INSERT INTO customer(customer_id , customer_name , email, address") VALUE('%s, %s, %s, %s'))
+mycursor.execute("INSERT INTO customer(customer_id , customer_name , email, address) VALUE('%s, %s, %s, %s)")
 VALUE(2,`Blessing Malik`,bmalik@sandtech.com`, `124 Happiness  Ave.`)
 VALUE( `3`,`Obed Ehoneah`,`eobed@sandtech.com`,`125 Happiness  Ave.`)
 VALUE(`4`, `Nehemial Kamolu`,`nkamolu@sandtech.com`,`126 Happiness  Ave.`)
